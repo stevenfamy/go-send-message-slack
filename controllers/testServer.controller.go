66aa06fc855a5fc0009e8b97b83213ddaeba954a
Steven Famy <stevenfamy@proton.me>
2023-09-07 13:44:24 +0700

    feat: add body payload for be and fe branch name in status api

--- controllers/testServer.controller.go
+++ controllers/testServer.controller.go
@@ -7,25 +7,24 @@ import (
 	"github.com/stevenfamy/go-send-message-slack/models"
 )
 
-// type UpdateBuild struct {
-// 	ProjectId string `json:"projectId"`
-// 	ServerId  string `json:"serverId"`
-// 	Name      string `json:"name"`
-// }
+type UpdateBuild struct {
+	FeBranch string `json:"feBranch"`
+	BeBranch string `json:"beBranch"`
+}
 
 func DoUpdateServerBuild(c *gin.Context) {
-	// var updateBuild UpdateBuild
+	var updateBuild UpdateBuild
 
 	projectId := c.Param("projectId")
 	serverId := c.Param("serverId")
 	name := c.Param("name")
 
-	// if err := c.ShouldBindJSON(&updateBuild); err != nil {
-	// 	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 	return
-	// }
+	if err := c.ShouldBindJSON(&updateBuild); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-	models.UpdateServerBuild(projectId, serverId, name)
+	models.UpdateServerBuild(projectId, serverId, name, updateBuild.FeBranch, updateBuild.BeBranch)
 
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 
--- models/testing_status.model.go
+++ models/testing_status.model.go
@@ -14,10 +14,12 @@ type TestingStatus struct {
 	Status          bool   `json:"status"`
 	StatusChangedBy string `json:"status_changed_by"`
 	StatusChangedOn int    `json:"status_changed_on"`
+	LastFeBranch    string `json:"last_fe_branch"`
+	LastBeBranch    string `json:"last_be_branch"`
 }
 
-func UpdateServerBuild(Project string, ServerId string, Name string) {
-	_, err := DB.Query("UPDATE testing_status set status = 1, last_build_by = ?, last_build_on = ? where project = ? and server_id = ?", Name, time.Now().Unix(), Project, ServerId)
+func UpdateServerBuild(Project string, ServerId string, Name string, FeBranch string, BeBranch string) {
+	_, err := DB.Query("UPDATE testing_status set status = 1, last_build_by = ?, last_build_on = ?, last_fe_branch = ?, last_be_branch = ? where project = ? and server_id = ?", Name, time.Now().Unix(), FeBranch, BeBranch, Project, ServerId)
 	if err != nil {
 		log.Print(err.Error())
 	}
